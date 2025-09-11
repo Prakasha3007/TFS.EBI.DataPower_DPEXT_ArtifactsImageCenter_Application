980265d1d44df8cb41a9f4e82e868c6712c76f96
Prakasha3007 <jayaprakasha3007@gmail.com>
2025-09-11 15:02:35 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Using Templates!");
+        System.out.println("Hello, Using Templates! Checking false");
     }
 }
